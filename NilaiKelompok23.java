d1fbd684d33d6ae01b7c378bd153c8a2a3e52c6c
mangeaksir <radiantrafie@gmail.com>
2025-10-31 13:12:51 +0700

    Studi Kasus Nilai Tugas Proyek Kelompok

--- NilaiKelompok23.java
+++ NilaiKelompok23.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
 public class NilaiKelompok23 {
     public static void main(String[] args) {
-        int i=1, j, nilai, kelompokTertinggi;
-        float totalNilai, rataNilai, rataTertinggi;
-    
+        int nilai, j;
+        float totalNilai, rataNilai;
         Scanner sc = new Scanner(System.in);
-        
+        int i = 1;
         while (i<=6) {
             System.out.println("Kelompok " + i);
             
